509d531f084e16218d2ae1aed5dea0c577051e37
Giga_aja <gigaananta07@gmail.com>
2025-10-13 09:10:58 +0700

    Update for upload file max

--- server.js
+++ server.js
@@ -36,8 +36,13 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+// Jika kamu pakai multer, pastikan storage-nya juga benar
+const upload = multer({
+    limits: { fileSize: 50 * 1024 * 1024 }
+});
 
 
 // setup dirname
