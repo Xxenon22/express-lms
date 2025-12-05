1bebfb82a7ed66dca583597c1f643a00d20b53bf
Giga_aja <gigaananta07@gmail.com>
2025-12-05 11:27:04 +0700

    New Update vers 2

--- config/db.js
+++ config/db.js
@@ -6,19 +6,19 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    // host: process.env.DB_HOST,
+    // port: process.env.DB_PORT,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_DATABASE
 
-    // host: process.env.PG_HOST,
-    // port: process.env.PG_PORT,
-    // user: process.env.PG_USER,
-    // password: process.env.PG_PASSWORD,
-    // database: process.env.PG_DATABASE
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE
 });
 
-console.log("Connecting to DB:", process.env.DB_HOST, process.env.DB_USER);
+console.log("Connecting to DB:", process.env.PG_HOST, process.env.PG_USER);
 
 export { pool };
