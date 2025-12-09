e463008bbe96cd95793162e54eaa6b39872961ab
Giga_aja <gigaananta07@gmail.com>
2025-12-09 11:19:27 +0700

    update env

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,18 +3,13 @@ import nodemailer from "nodemailer";
 export default async function sendEmail({ to, subject, text }) {
     try {
         const transporter = nodemailer.createTransport({
-            host: "smtp-relay.brevo.com",
+            host: "smtp.gmail.com",
             port: 2525,
             secure: false,
             auth: {
                 user: process.env.EMAIL,
                 pass: process.env.EMAIL_PASS,
             },
-            tls: {
-                rejectUnauthorized: false // supaya tidak gagal di server VPS dengan sertifikat tertentu
-            },
-            connectionTimeout: 10000, // 10 detik timeout
-            family: 4   // paksa pakai IPv4
         });
 
         const info = await transporter.sendMail({
