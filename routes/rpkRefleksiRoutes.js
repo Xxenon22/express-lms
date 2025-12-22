8e5862078990db74d6c587c082a7fd12a6327f5c
Giga_aja <gigaananta07@gmail.com>
2025-12-22 12:04:41 +0700

    fixed bug rpk

--- routes/rpkRefleksiRoutes.js
+++ routes/rpkRefleksiRoutes.js
@@ -15,12 +15,9 @@ router.get("/", verifyToken, async (req, res) => {
              u.username AS teacher_name,
              dg.name AS instructor_name,
              g.grade_lvl AS name_grade,
-             m.nama_jurusan AS major,
-             dm.nama_mapel AS subject
+             m.nama_jurusan AS major
       FROM rpk_refleksi rr
       LEFT JOIN rombel rb ON rr.rombel_id = rb.id
-      LEFT JOIN kelas k ON k.id = rr.kelas_id
-      LEFT JOIN db_mapel dm ON dm.id = k.id_mapel
       LEFT JOIN users u ON rr.guru_id = u.id
       LEFT JOIN db_guru dg ON rr.instructor = dg.id
       LEFT JOIN grade_level g ON rb.grade_id = g.id
@@ -81,7 +78,7 @@ router.get("/:id", async (req, res) => {
             FROM rpk_refleksi rr
             LEFT JOIN rombel rb ON rr.rombel_id = rb.id
             LEFT JOIN kelas k ON k.rombel_id = rb.id      -- ✅ tambahkan join ke kelas
-            LEFT JOIN db_mapel dm ON k.id_mapel = dm.id  -- ✅ ambil subject
+            LEFT JOIN db_mapel dm ON dm.id = k.id_mapel  -- ✅ ambil subject
             LEFT JOIN users u ON rr.guru_id = u.id
             LEFT JOIN db_guru dg ON rr.instructor = dg.id
             LEFT JOIN grade_level g ON rb.grade_id = g.id
