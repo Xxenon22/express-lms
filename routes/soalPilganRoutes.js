cab6efd32af2d7707f44a149bd7874a54dd12478
Giga_aja <gigaananta07@gmail.com>
2025-12-29 16:10:28 +0700

    fetching materi

--- routes/soalPilganRoutes.js
+++ routes/soalPilganRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { pool } from "../config/db.js";
+import { verifyToken } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 // Tambah soal (bisa banyak sekaligus)
