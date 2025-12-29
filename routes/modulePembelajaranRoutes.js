6a439709dd5eb20f7df096c6a9d29b11e3dcb36b
Giga_aja <gigaananta07@gmail.com>
2025-12-29 11:45:38 +0700

    switching file storage

--- routes/modulePembelajaranRoutes.js
+++ routes/modulePembelajaranRoutes.js
@@ -167,7 +167,7 @@ router.get("/kelas/:kelasId", async (req, res) => {
                 mp.created_at,
                 bs.id AS bank_soal_id
              FROM module_pembelajaran mp
-             LEFT JOIN bank_soal bs ON mp.id = bs.module_id
+             LEFT JOIN bank_soal bs ON mp.id = bs.bank_soal_id
              WHERE mp.kelas_id = $1
              ORDER BY mp.created_at DESC`,
             [kelasId]
