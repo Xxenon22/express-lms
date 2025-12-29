f918521c3910cdc5fdc7db8804033cbea7868cdc
Giga_aja <gigaananta07@gmail.com>
2025-12-29 16:22:45 +0700

    fetching materi

--- server.js
+++ server.js
@@ -38,8 +38,9 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 // Jika kamu pakai multer, pastikan storage-nya juga benar
 // const upload = multer({
