ab97588f6f1c83b2f765b8c73973c5898cbaaa55
Giga_aja <gigaananta07@gmail.com>
2026-01-09 21:32:33 +0700

    add maintanance feature

--- middleware/maintanceMiddleware.js
+++ middleware/maintanceMiddleware.js
@@ -3,16 +3,13 @@ import { pool } from "../config/db.js";
 const maintenanceMiddleware = async (req, res, next) => {
     try {
         const result = await pool.query(`
-            SELECT status
-            FROM maintenance
+            SELECT status FROM maintenance
             LIMIT 1
         `);
 
-        if (result.rows[0]?.is_active) {
+        if (result.rows[0]?.status) {
             return res.status(503).json({
-                maintenance: true,
-                title: result.rows[0].title,
-                message: result.rows[0].message
+                maintenance: true
             });
         }
 
