66bfa18c51d5545720154b573cbaa12eb889095d
Giga_aja <gigaananta07@gmail.com>
2026-01-11 08:49:07 +0700

    maintance

--- routes/rpkRefleksiRoutes.js
+++ routes/rpkRefleksiRoutes.js
@@ -92,7 +92,7 @@ router.get("/:id", verifyToken, async (req, res) => {
              g.grade_lvl AS name_grade,
              j.nama_jurusan AS major
       FROM rpk_refleksi rr
-      LEFT JOIN rombel rb ON rb.id = rr.rombel_id
+      LEFT JOIN rombel rb ON rr.rombel_id = rb.id
       LEFT JOIN kelas k ON k.id = rr.kelas_id
       LEFT JOIN db_mapel dm_kelas ON dm_kelas.id = k.id_mapel
       LEFT JOIN db_mapel dm_lama ON dm_lama.id = rr.mapel_id
